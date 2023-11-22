e57cec7ca126a33847034fe40024c351a8acdeb0
Prathamesh2903 <121375680+Prathamesh2903@users.noreply.github.com>
2023-11-22 18:39:26 +0000

    datatypes in js

---
+++ Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") //using nodejs not browser
+
+console.log(3
+    +
+    3)// not a good practice
+
+console.log("prathamesh")
+
+let name = "hello js"
+let age = 18
+let isLoggedIn = false
+
+// number => 2^53
+// bigint 
+// string => prefer double quotes ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof "hello js")
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
