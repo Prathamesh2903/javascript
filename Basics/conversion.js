14d3b689ef69439e0303777e4912478fd8b8f0af
Prathamesh2903 <121375680+Prathamesh2903@users.noreply.github.com>
2023-11-22 18:54:23 +0000

    conversion of dataTypes

---
+++ Basics/conversion.js
@@ -0,0 +1,22 @@
+let score = "29ab" 
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1  false => 0
+//null => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedin = Boolean(isLoggedIn)
+console.log(booleanIsLoggedin)
+
+// 1=>true 0=>false
+// ""=> false "asf"=>true
+
+let boolNum = true;
+let stringNum = String(boolNum)
+console.log(typeof stringNum)
