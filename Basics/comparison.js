9462c7e0a6f0a2bb2876c3ed7c6aefa363966490
Prathamesh2903 <121375680+Prathamesh2903@users.noreply.github.com>
2023-12-22 08:30:44 +0000

    learnt more about conversions and comparisons

---
+++ Basics/comparison.js
@@ -0,0 +1,10 @@
+console.log("2" > 3)
+
+console.log(null > 0)  // false
+console.log(null == 0) // false
+console.log(null >= 0) // true 
+
+// comparison operator and equality operator work differently
+
+console.log("2" == 2) // true
+console.log("2" === 2) // false -> strict check with 3 equality
--- Basics/conversion.js
+++ Basics/conversion.js
@@ -1,22 +1,44 @@
-let score = "29ab" 
-console.log(typeof score);
+// let score = "29ab" 
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-//"33" => 33
-//"33abc" => NaN
-//true => 1  false => 0
-//null => 0
+// //"33" => 33
+// //"33abc" => NaN
+// //true => 1  false => 0
+// //null => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedin = Boolean(isLoggedIn)
-console.log(booleanIsLoggedin)
+// let isLoggedIn = 1
+// let booleanIsLoggedin = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedin)
 
-// 1=>true 0=>false
-// ""=> false "asf"=>true
+// // 1=>true 0=>false
+// // ""=> false "asf"=>true
 
-let boolNum = true;
-let stringNum = String(boolNum)
-console.log(typeof stringNum)
+// let boolNum = true;
+// let stringNum = String(boolNum)
+// console.log(typeof stringNum)
+
+//*****operations*********
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1+2+"2") // 32
+console.log("1"+2+2) // 122
+
+console.log(true) // true
+console.log(+true) //1
+
+let gameCounter = 99
+gameCounter++
+console.log(gameCounter++) // use and increment
+console.log(++gameCounter) // increment and use
