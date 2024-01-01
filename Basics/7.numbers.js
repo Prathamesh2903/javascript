ef447eefe5daad81eafbcf6e97a75f5736903850
Prathamesh2903 <121375680+Prathamesh2903@users.noreply.github.com>
2024-01-01 14:57:44 +0000

    Learnt About Number And Maths

---
+++ Basics/7.numbers.js
@@ -0,0 +1,25 @@
+// const score = 400
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(score)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.654568
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+console.log(Math.abs(-10));
+console.log(Math.round(4.3));
+console.log(Math.sqrt(30).toFixed(2));
+
+
+console.log(Math.random()); // always between 0 and 1
+
+console.log((Math.random()*10).toPrecision(1));
+
