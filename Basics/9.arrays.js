362fa22ad6e033afd6ce11b6655ad5eb369b2459
Prathamesh2903 <121375680+Prathamesh2903@users.noreply.github.com>
2024-01-02 08:26:02 +0000

    Array Functions

--- Basics/9.arrays.js
+++ Basics/9.arrays.js
@@ -1,10 +1,27 @@
-const myArr = [0, 1 , 2 , 3]
+const myArr = [0, 1 , 2 , 3 , 4 , 5]
 console.log(myArr[1]);
 
 const arr = new Array(1,2,3,4)
 
 // Array methods 
 myArr.push(69)
-console.log(myArr);
+//console.log(myArr);
 myArr.pop()
+
+myArr.unshift(10);
+myArr.shift();
 console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+
+const newArr = myArr.join() // newArr now is a string  
+console.log(newArr);
+
+console.log(myArr);
+const arr1 = myArr.slice(1,3); // no change in the original array
+console.log(arr1);
+
+const arr2 = myArr.splice(1,3); // original array changed -> myArr = [0 , 4 , 5]
+console.log(myArr);
+console.log(arr2);
