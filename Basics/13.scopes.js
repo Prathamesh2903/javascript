ff4ecd4a2c37c021cc832e245ceb657bcf74c015
Prathamesh2903 <121375680+Prathamesh2903@users.noreply.github.com>
2024-01-08 17:13:28 +0000

    scope and mini-hoisting

--- Basics/13.scopes.js
+++ Basics/13.scopes.js
@@ -1,8 +1,47 @@
-if(true){
-    let a = 20 // locally
-    const b = 30 // locally
-    var c = 40  // declared globally
+// if(true){
+//     let a = 20 // locally
+//     const b = 30 // locally
+//     var c = 40  // declared globally
+// }
+
+// console.log(a,b,c)
+
+function one(){
+    const userName = "prathamesh"
+
+    function two(){
+        const webSite = "youtube"
+        console.log(userName) // accessible
+    }
+
+    //console.log(webSite) // not accessible
+
+    two()
 }
 
-console.log(a,b,c)
+one()
+
+if(true){
+    const userName = "prathamesh"
+
+    if(userName === "prathamesh"){
+        const website = " youtube"
+        console.log(userName + website)
+    }
+
+}
+
+// scope -> {.....}
+
+//***************interesting**************
+
+console.log(incrementValue(5)); // will give the output (hoisting)
+function incrementValue(num){
+    return num + 1
+}
+
+addTwo(5) // will throw error -> expression format
+const addTwo = function(num){
+    return num + 2
+}
 
